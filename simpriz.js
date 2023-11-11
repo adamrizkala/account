252d36aeaa48011de4fa712ecbdc3b9e006a4ec0
Adam Rizkala <132635745+adamrizkala@users.noreply.github.com>
2023-11-11 13:46:11 +0700

    Update simpriz.js

--- simpriz.js
+++ simpriz.js
@@ -1,6 +1,9 @@
-const host = location.hostname
-if (host !== "ustadadamrizkal.blogspot.com"){
-    document.body.innerHTML = "<div style='font-size: 100px; height: 100%; width: 100%; display: grid; justify-content: center; align-content: center;'>Domain Anda belum terdaftar, silahkan hubungi WA 085727754016</div>";
-} if else (host !== "ustadadamrizkala.blogspot.com"){
-    document.body.innerHTML = "<div style='font-size: 100px; height: 100%; width: 100%; display: grid; justify-content: center; align-content: center;'>Domain Anda belum terdaftar, silahkan hubungi WA 085727754016</div>";
-}
+const hostname = location.hostname;
+const inner = "<div style='width: 100%;display: grid;justify-content: center;align-content: center; padding: 30px; height: 100vh;'><p style='font-size: 30px;text-align: center;'>Domain Anda belum terdaftar, silahkan melakukan regristasi melalui WA dengan mengklik link berikut ini : <a href='https://api.whatsapp.com/send?phone=6285727754016'>085727754016</a></p></div>";
+const account = ["ustadadamrizkala.blogspot.com", "nasehatquran.com", "adam rizkala"];
+const searchAccount = account.filter(value => value === hostname);
+let count = 0;
+
+if (searchAccount[0] !== hostname){
+    document.body.innerHTML = inner;
+};
