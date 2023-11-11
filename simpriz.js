715861b3a6001704a51356635a9e7ff32794f70a
Adam Rizkala <132635745+adamrizkala@users.noreply.github.com>
2023-11-11 14:01:45 +0700

    Update simpriz.js

--- simpriz.js
+++ simpriz.js
@@ -1,5 +1,5 @@
 const hostname = location.hostname;
-const inner = "<div style='width: 100%;display: grid;justify-content: center;align-content: center; padding: 30px; height: 100vh;'><p style='font-size: 30px;text-align: center;'>Mohon maaf! Anda mendapatkan template ini secara ilegal. Silahkan beli template ini melalui WA dengan mengklik link berikut ini : <a href='https://api.whatsapp.com/send?phone=6285727754016'>085727754016</a></p></div>";
+const inner = "<div style='width: 100%;display: grid;justify-content: center;align-content: center; padding: 30px; height: 100vh;'><p style='font-size: 30px;text-align: center;'>Mohon maaf! Anda mendapatkan template ini secara ilegal. Silahkan beli template ini melalui WA dengan mengklik link berikut ini : <a href='https://wa.me/6285727754016?&text=Halo%2C%20kak!%20Saya%20ingin%20membeli%20template%20%22simpriz%22.%20Berikut%20alamat%20blog%20saya%20%3A%20%5Bwww.alamatblogsaya.com%5D'>KLIK!</a></p></div>";
 const account = ["ustadadamrizkal.blogspot.com", 
                  "www.nasehatquran.com"];
 const searchAccount = account.filter(value => value === hostname);
