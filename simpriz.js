d775c25060913ef987e7acecab420417d2212d0f
Adam Rizkala <132635745+adamrizkala@users.noreply.github.com>
2023-11-11 10:14:09 +0700

    Update simpriz.js

--- simpriz.js
+++ simpriz.js
@@ -1,3 +1,3 @@
-if (location.hostname !== "www.nasehatquran.com"){
+if (document.getElementById('kode-pendaftaran').childNodes[0].childNodes[1].innerText !== "1234567890"){
     document.body.innerHTML = null;
 }
