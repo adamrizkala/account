577a36099e496667da59bb758954c7e905d81329
Adam Rizkala <132635745+adamrizkala@users.noreply.github.com>
2023-11-11 10:30:47 +0700

    Update simpriz.js

--- simpriz.js
+++ simpriz.js
@@ -1,4 +1,6 @@
-const kode = document.getElementById('kode-pendaftaran').childNodes[0].childNodes[1].innerText;
-if (kode !== "1234567890"){
-    document.body.innerHTML = null;
+const host = location.hostname
+if (host !== "ustadadamrizkal.blogspot.com"){
+    document.body.innerHTML = "<div style='font-size: 100px; height: 100%; width: 100%; display: grid; justify-content: center; align-content: center;'>Domain Anda belum terdaftar, silahkan hubungi WA 085727754016</div>";
+} if else (host !== "ustadadamrizkala.blogspot.com"){
+    document.body.innerHTML = "<div style='font-size: 100px; height: 100%; width: 100%; display: grid; justify-content: center; align-content: center;'>Domain Anda belum terdaftar, silahkan hubungi WA 085727754016</div>";
 }
