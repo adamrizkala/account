32d97fc6acd50b05d45637f9de51c989ed611b07
Adam Rizkala <132635745+adamrizkala@users.noreply.github.com>
2023-11-12 13:29:36 +0700

    Update simpriz.js

--- simpriz.js
+++ simpriz.js
@@ -30,7 +30,7 @@ document.getElementById('Image2').id = "Image1";
 document.getElementById('LinkList2').id = "LinkList1";
 document.getElementById('FeaturedPost2').id = "FeaturedPost1";
 document.getElementById('Blog2').id = "Blog1";
-document.getElementById('PopularPosts2').id = "PopularPosts2";
+document.getElementById('PopularPosts2').id = "PopularPosts1";
 document.getElementById('Label2').id = "Label1";
 const hostname = location.hostname;
 const inner = `<div style='width: 100%;display: grid;justify-content: center;align-content: center; padding: 30px; height: 100vh;'><p style='font-size: 30px;text-align: center;'>Mohon maaf! Anda mendapatkan template ini secara ilegal. Silahkan beli template ini melalui WA dengan mengklik link berikut ini : <a href='https://wa.me/6285727754016?&text=Halo%2C%20kak!%20Saya%20ingin%20membeli%20template%20%22simpriz%22.%20Berikut%20alamat%20blog%20saya%20%3A%20%5Bwww.alamatblogsaya.com%5D'><button style="cursor: pointer;font-size: 30px;padding: 5px 20px;border-radius: 10px;">KLIK!</button></a></p></div>`;
